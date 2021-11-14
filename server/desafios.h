901e7604cdeca251206a1c011e666da22baa8188
jnegroITBA <jnegro@itba.edu.ar>
2021-11-14 19:11:13 -0300

    Primeros desafíos

--- server/desafios.h
+++ server/desafios.h
@@ -1,4 +1,4 @@
-#define DESAFIO_COUNT 3
+#define DESAFIO_COUNT 11
 
 typedef void (*DesafioFunc)();
 
@@ -13,6 +13,14 @@ void desafio3();
 
 static Desafio desafios[DESAFIO_COUNT] = {
     {"entendido", (DesafioFunc)desafio1},
-    {"https://www.youtube.com/watch?v=dQw4w9WgXcQ", (DesafioFunc)desafio2},
-    {"coto", (DesafioFunc)desafio3}
+    {"https://www.youtube.com/watch?v=dQw4w9WgXcQ", (DesafioFunc)desafio2}, // Imagen
+    {"42", (DesafioFunc)desafio3}, //EBADF
+    {}, //strings:277
+    {}, //.RUN_ME
+    {}, //Filter error
+    {}, //Fondo en negro
+    {}, //Latex
+    {}, //quine.c
+    {}, //b gdme
+    {}, //normal
 };
